6bffd898eab99ce848c51061fe77f6b11067c106
Tomoya Koyanagi <tomk79@gmail.com>
2015-08-20 12:45:05 +0900

    大きいものから小さいものまで作ってみた。

---
+++ README.md
@@ -0,0 +1,11 @@
+# _test_large_sitemap
+
+[Pickles2](http://pickles2.pxt.jp/) に大きいサイズのサイトマップを処理するテストのための、`sitemap.csv` を生成するツールです。
+
+## Usage
+
+```
+$ php generateCsv.php "/?max=10000&bros=5&depth=6"
+```
+
+`/dist` の中にCSVファイルが出力されます。
--- generateCsv.php
+++ generateCsv.php
@@ -3,21 +3,36 @@ require_once(__DIR__.'/vendor/autoload.php');
 
 class main{
     private $fs;
+    private $req;
     private $conf;
     private $path_sitemapCsv;
     private $page_numer = 0;
 
     public function __construct(){
         $this->fs = new \tomk79\filesystem();
-        $this->path_sitemapCsv = __DIR__.'/sitemap.csv';
-        if( is_file($this->path_sitemapCsv) ){
-            unlink($this->path_sitemapCsv);
-        }
-        $this->conf = new stdClass;
-        $this->conf->bros = 10;
-        $this->conf->depth = 4;
-        $this->conf->max_number = 10000;
+        $this->req = new \tomk79\request();
 
+        $this->path_sitemapCsv = __DIR__.'/dist/';
+        if( !is_dir($this->path_sitemapCsv) ){
+            $this->fs->mkdir($this->path_sitemapCsv);
+        }
+
+        $this->conf = new stdClass;
+        $this->conf->bros = 5;
+        $this->conf->depth = 6;
+        $this->conf->max = 5000;
+
+        if( $this->req->get_param('max') ){
+            $this->conf->max = intval( $this->req->get_param('max') );
+        }
+        if( $this->req->get_param('bros') ){
+            $this->conf->bros = intval( $this->req->get_param('bros') );
+        }
+        if( $this->req->get_param('depth') ){
+            $this->conf->depth = intval( $this->req->get_param('depth') );
+        }
+
+        $this->csv_filename = 'sitemap_max'.intval($this->conf->max).'_d'.intval($this->conf->depth).'_b'.intval($this->conf->bros).'.csv';
     }
 
     public function kick(){
@@ -26,6 +41,7 @@ class main{
     }
 
     private function init(){
+        $this->fs->rm( $this->path_sitemapCsv.$this->csv_filename );
         $this->page_numer = 0;
         $csv = array();
         array_push($csv, array(
@@ -62,13 +78,13 @@ class main{
             "description" => "",
             "category_top_flg" => "1",
         ));
-        error_log( $this->fs->mk_csv($csv), 3, $this->path_sitemapCsv );
+        error_log( $this->fs->mk_csv($csv), 3, $this->path_sitemapCsv.$this->csv_filename );
     }
 
     private function execute($breadcrumb = '', $depth = 0){
 
         for( $iBros = 0; $iBros<$this->conf->bros; $iBros++ ){
-            if($this->page_numer > $this->conf->max_number){
+            if($this->page_numer > $this->conf->max){
                 break;
             }
             $csv = array();
@@ -90,8 +106,8 @@ class main{
                 "description" => "",
                 "category_top_flg" => (strlen($breadcrumb)?'':'1'),
             ));
-            var_dump($csv);
-            error_log( $this->fs->mk_csv($csv), 3, $this->path_sitemapCsv );
+            var_dump('/page/page_'.$this->page_numer.'.html');
+            error_log( $this->fs->mk_csv($csv), 3, $this->path_sitemapCsv.$this->csv_filename );
             $this->page_numer ++;
 
             if($depth < $this->conf->depth){
